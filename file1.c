87b4d371b91eb379d644cb20c7f7d0e9ed3b4921
hongchal <chal405@naver.com>
2021-03-03 11:12:56 +0900

    got the bug!

--- file1.c
+++ file1.c
@@ -3,7 +3,8 @@
 int main(){
 	printf("this is file1\n");
 	printf("hi i'm file2\n");
-	printf("new line in mster!\n");
+	printf("new line in master!\n");
+	printf("i got the bug!\n");
 	return 0;
 }
 
